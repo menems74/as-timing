6873bc0991941caad8f323fde749a4e7b2f576dd
Remolo Ceni <menems@gmail.com>
2026-07-17 09:20:14 +0200

    Collega configurazione Firebase reale e inizializza Auth/Firestore

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,10 +2,10 @@
 // Questi valori si trovano in: Console Firebase > Impostazioni progetto > Le tue app > Web app.
 // Sono pubblici per natura (protezione reale affidata alle Firestore Security Rules).
 export const firebaseConfig = {
-  apiKey: "INSERISCI_API_KEY",
-  authDomain: "INSERISCI_PROJECT_ID.firebaseapp.com",
-  projectId: "INSERISCI_PROJECT_ID",
-  storageBucket: "INSERISCI_PROJECT_ID.appspot.com",
-  messagingSenderId: "INSERISCI_SENDER_ID",
-  appId: "INSERISCI_APP_ID",
+  apiKey: "AIzaSyD8pCgZqX-zeSZbwwbZtWVKcVHzjd2oooQ",
+  authDomain: "as-timing.firebaseapp.com",
+  projectId: "as-timing",
+  storageBucket: "as-timing.firebasestorage.app",
+  messagingSenderId: "912409497698",
+  appId: "1:912409497698:web:e0936b47fbcf389bd5013b",
 };
